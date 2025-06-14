3e6c7a8a341dc3334370a91389fadb2ca1dbb9b7
icawaeo <veronica.waeo@gmail.com>
2025-06-15 00:48:18 +0800

    enable CORS for vercel frontend

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'framework2-frontend.vercel.app', 
+    origin: 'https://framework2-theta.vercel.app', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
