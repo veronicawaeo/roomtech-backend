2959351ae7419ed665cdb904543d6c3881f5ed0e
icawaeo <veronica.waeo@gmail.com>
2025-06-15 01:39:20 +0800

    update json

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://framework2-theta.vercel.app', 
+    origin: true, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
